264c9e3121f1204157eff21991ebb66630aada3f
guruh <guruh@gmail.com>
2024-08-25 00:06:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function Welcome(){
+  const style = {
+    backgroundColor: 'black',
+    color: 'white', // Optional: changes text color to white for better visibility
+    padding: '20px' // Optional: adds some padding for better spacing
+  };
+
+  return (
+    <div style={style}>
+      <h1>Hi, it's me Guru H</h1>
+      <p>Thanks for viewing my profile</p>
+    </div>
+)
+}
+
+export default Welcome
